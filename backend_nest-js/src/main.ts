35ae53b39a8817279d2eb92cea8dd54c57247abf
dangnhatminh <dangnhatminh09032002@gmail.com>
2022-05-11 19:11:25 +0700

    temp

--- backend_nest-js/src/main.ts
+++ backend_nest-js/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   });
--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const fetch = axios.create({
-  baseURL: process.env.AXIOS_BASE_URL || "http://localhost:8080",
+  baseURL: process.env.REACT_APP_AXIOS_BASE_URL,
+  headers: {
+    Accept: "application/json",
+    Access_Control_Allow_Origin: "*",
+  },
   withCredentials: true,
 });
 
